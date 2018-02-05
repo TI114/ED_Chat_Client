5125e6e6e2740116438f6bed997dcd1fb440c91e
Martin Schäfer <martin.schaefer.ms@outlook.de>
2018-02-05 20:18:15 +0100

    Projektdateien hinzufügen.

---
+++ chat_client/ClientForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace chat_client
+{
+    public partial class ClientForm : Form
+    {
+        TClient client;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+            btnSend.Hide();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // client mit der IP aus der Eingabzeile erstellen
+            client = new TClient(this, tbInput.Text);
+            //btnConnect.Hide();
+            btnSend.Show();
+        }
+
+        // Delegat-Typ für Thread-übergreifenden Methodenaufruf
+        delegate void InvokeDelegate (string s);
+
+        // Diese Methode wird aus dem Thread aufgerufen
+        public void showClientMessage(string msg)
+        {
+            // Wenn unsicherer Zustand
+            if (InvokeRequired)
+                BeginInvoke(new InvokeDelegate(showClientMessage), msg);
+            else
+                // sonst direktes Schreiben möglich
+                lbMessage.Items.Add(msg);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // JSON: "FromC":"ClientName","ToC":"ClientName","MSG":"nachricht"
+            string msgText = "{\"FromC\":\"" + tbusername.Text + "\",\"ToC\":\"" + tbempfaenger.Text + "\",\"MSG\":\"" + tbInput.Text + "\"}";
+
+            //string msgText = Newtonsoft.Json.JsonConvert.SerializeObject();
+
+            // über den client an den Server schicken
+            client.sendMessage(msgText);
+
+        }
+
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Text = "Closing ...";
+            if (client != null)
+            {
+                client.sendMessage("<q>");
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ chat_client/ClientForm.designer.cs
@@ -0,0 +1,123 @@
+﻿namespace chat_client
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lbMessage = new System.Windows.Forms.ListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbusername = new System.Windows.Forms.TextBox();
+            this.tbempfaenger = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(72, 113);
+            this.tbInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(476, 22);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.Text = "127.0.0.1";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(72, 156);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 28);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.FormattingEnabled = true;
+            this.lbMessage.ItemHeight = 16;
+            this.lbMessage.Location = new System.Drawing.Point(72, 203);
+            this.lbMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(476, 212);
+            this.lbMessage.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(180, 156);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 28);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbusername
+            // 
+            this.tbusername.Location = new System.Drawing.Point(72, 35);
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(182, 22);
+            this.tbusername.TabIndex = 4;
+            // 
+            // tbempfaenger
+            // 
+            this.tbempfaenger.Location = new System.Drawing.Point(366, 35);
+            this.tbempfaenger.Name = "tbempfaenger";
+            this.tbempfaenger.Size = new System.Drawing.Size(182, 22);
+            this.tbempfaenger.TabIndex = 5;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 467);
+            this.Controls.Add(this.tbempfaenger);
+            this.Controls.Add(this.tbusername);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbInput);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ClientForm";
+            this.Text = "ClientForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClientForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lbMessage;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbusername;
+        private System.Windows.Forms.TextBox tbempfaenger;
+    }
+}
